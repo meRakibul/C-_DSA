d871f1d91e3c809baac98dcfc90fe6d6c5be6359
meRakibul <hralibul@gmail.com>
2024-08-26 17:45:08 +0600

    first commit

---
+++ if else conditon/first.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int ;
+    cin>>n;
+    if (n%2==0)
+    {
+         cout<<"Number is even";
+    }else{
+        cout<<"Number is odd";
+    }
+    
+    
+   
+    
+    return 0;
+}
