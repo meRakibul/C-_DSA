5ee81c9167208b219cf589f68e9b4ba3f93e6eb7
meRakibul <hralibul@gmail.com>
2024-08-26 17:50:40 +0600

    added max

---
+++ if else conditon/max_min.cpp
@@ -0,0 +1,33 @@
+//this is for max
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+     cin>>a>>b>>c;
+     if (a>b)
+     {
+        if (a>c)
+
+        {
+          cout<<a<<endl;  /* code */
+        }else{
+            cout<<c<<endl;
+        }
+        
+     }else{
+        if (b>c)
+        {
+            cout<<b<<endl;
+        }else{
+            cout<<c<<endl;
+        }
+        
+     }
+     
+    
+   
+    
+    return 0;
+}
